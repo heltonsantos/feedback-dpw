df998fb9391d9d6abc5f4d03e4ce47778a6e70d7
Helton Douglas Araújo dos Santos <hdas@cin.ufpe.br>
2015-12-12 12:58:07 -0300

    Configurado o projeto para o WildFly Application Server 8.2.1.Final

--- datafeed/src/main/java/com/br/datafeed/rest/filter/CORSFilter.java
+++ datafeed/src/main/java/com/br/datafeed/rest/filter/CORSFilter.java
@@ -7,7 +7,7 @@ import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 
-//@Provider
+@Provider
 public class CORSFilter implements ContainerResponseFilter {
 
    public void filter(final ContainerRequestContext requestContext,
